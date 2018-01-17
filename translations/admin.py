fbfa0fac1000b5c8ad6ae8794cd5488903b419f6
Martin Nobis <martin.nobis@gmail.com>
2018-01-17 22:45:22 +1100

    Created super user. Added postgresql settings. Used inspectdb to import tables to models.

--- translations/admin.py
+++ translations/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from translations.models import English, Latvian, EnLv
 
 # Register your models here.
+admin.site.register(English)
+admin.site.register(Latvian)
+admin.site.register(EnLv)
--- translations/models.py
+++ translations/models.py
@@ -1,3 +1,43 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
-# Create your models here.
+class EnLv(models.Model):
+    en = models.ForeignKey('English', models.CASCADE, blank=True, null=True)
+    lv = models.ForeignKey('Latvian', models.CASCADE, blank=True, null=True)
+    id = models.BigAutoField(primary_key=True)
+
+    class Meta:
+        managed = False
+        db_table = 'en_lv'
+
+    def __str__(self):
+        return "%s = %s" % (self.en.txt, self.lv.txt)
+
+class English(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    txt = models.TextField(unique=True, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'english'
+
+    def __str__(self):
+        return self.txt
+
+
+class Latvian(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    txt = models.TextField(unique=True, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'latvian'
+
+    def __str__(self):
+        return self.txt
