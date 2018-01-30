5caca26c0a9e4f48e0ff4543ab33b3585deccee2
Martin Nobis <martin.nobis@gmail.com>
2018-01-30 22:51:36 +1100

    Updated allowed host for jenkins running on raspberry pi 3.

--- dictlv/settings.py
+++ dictlv/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-j%%h$g^u8^!y5ei3d!$&gz$fpvkq&ws6+*_lb4w2_4u7%ut$9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.7']
 
 
 # Application definition
